1bc2571d673b3e5235211e503848783120544f16
Viet Nguyen <inbox@dev-forge.ru>
2017-11-23 00:11:31 +0300

    feat: move canvas

--- main.lua
+++ main.lua
@@ -14,6 +14,13 @@ local pointer = {
   y = display.contentCenterY
 }
 
+local origin = {
+  x = 0,
+  y = 0
+}
+
+local scrollSpeed = 100
+
 
 if not system.hasEventSource("gyroscope") then
   local txtOptions = {
@@ -40,6 +47,14 @@ local function networkListener(event)
   end
 end
 
+local function sendRaw(msg)
+  local params = {
+    body = json.encode(msg)
+  }
+
+  network.request("http://92.53.78.222:8080/events", "POST", networkListener, params)
+end
+
 local function sendMessage(type)
   local whiteboardEvent = {
     x = pointer.x,
@@ -47,13 +62,8 @@ local function sendMessage(type)
     type = type
   }
 
-  local params = {
-    body = json.encode(whiteboardEvent)
-  }
-
-  network.request("http://192.168.0.46:8080/events", "POST", networkListener, params)
+  sendRaw(whiteboardEvent)
 end
-
 
 local drawBtnPressed = false
 local firstDrawBtnPress = true
@@ -70,37 +80,127 @@ local drawBtn = widget.newButton({
 
 
 local function onClear()
+  origin.x = 0
+  origin.y = 0
+
+  pointer.x = 0
+  pointer.y = 0
+
   sendMessage('CLEAR')
 end
 
 local function onReset()
-  pointer.x = 0
-  pointer.y = 0
+  pointer.x = -origin.x
+  pointer.y = -origin.y
 
-  sendMessage('RESET')
+  sendMessage('MOVE_POINTER')
 end
 
 local clearBtn = widget.newButton({
   left = 10,
   top = 10,
   shape = "roundedRect",
-  label = "clear",
-  width = 100,
+  label = "clr",
+  width = 50,
   height = 50,
   onPress = onClear
 })
 
 
 local resetBtn = widget.newButton({
-  left = display.contentWidth - 110,
+  left = display.contentWidth - 60,
   top = 10,
   shape = "roundedRect",
-  label = "reset",
-  width = 100,
+  label = "clbr",
+  width = 50,
   height = 50,
   onPress = onReset
 })
 
+local sensitivity = 5
+
+local function sensitivitySliderListener(event)
+  sensitivity = event.value / 100 * 20
+end
+
+local sensitivitySlider = widget.newSlider({
+  left = 10,
+  top = 80,
+  width = display.contentWidth - 20,
+  value = 25,
+  listener = sensitivitySliderListener
+})
+
+
+local function scrollX(delta)
+  pointer.x = pointer.x - delta
+  origin.x = origin.x + delta
+
+  local scrollEvent = {
+    x = pointer.x,
+    y = pointer.y,
+    dx = delta,
+    dy = 0,
+    type = 'MOVE_CANVAS'
+  }
+
+  sendRaw(scrollEvent)
+end
+
+local function scrollY(delta)
+  pointer.y = pointer.y - delta
+  origin.y = origin.y + delta
+
+  local scrollEvent = {
+    x = pointer.x,
+    y = pointer.y,
+    dx = 0,
+    dy = delta,
+    type = 'MOVE_CANVAS'
+  }
+
+  sendRaw(scrollEvent)
+end
+
+local scrollLeftBtn = widget.newButton({
+  x = 35,
+  y = display.contentCenterY,
+  shape = "roundedRect",
+  label = "<",
+  width = 50,
+  height = 100,
+  onPress = function() scrollX(scrollSpeed) end
+})
+
+local scrollRightBtn = widget.newButton({
+  x = display.contentWidth - 35,
+  y = display.contentCenterY,
+  shape = "roundedRect",
+  label = ">",
+  width = 50,
+  height = 100,
+  onPress = function() scrollX(-scrollSpeed) end
+})
+
+local scrollUpBtn = widget.newButton({
+  left = display.contentCenterX - 50,
+  top = 10,
+  shape = "roundedRect",
+  label = "^",
+  width = 100,
+  height = 50,
+  onPress = function() scrollY(-scrollSpeed) end
+})
+
+local scrollDownBtn = widget.newButton({
+  left = display.contentCenterX - 50,
+  top = display.contentHeight - 60,
+  shape = "roundedRect",
+  label = "v",
+  width = 100,
+  height = 50,
+  onPress = function() scrollY(scrollSpeed) end
+})
 
 local function onGyro(event)
   local dx = event.xRotation * event.deltaTime * 180 / math.pi
@@ -109,30 +209,34 @@ local function onGyro(event)
   local changed = false
 
   if (math.abs(dx) > 0.1) then
-    pointer.y = pointer.y + dx * -5
+    pointer.y = pointer.y + dx * -sensitivity
     changed = true
   end
 
   if (math.abs(dz) > 0.1) then
-    pointer.x = pointer.x + dz * -5
+    pointer.x = pointer.x + dz * -sensitivity
     changed = true
   end
 
   if changed then
-    local msgType = 'MOVE_POINTER'
+    local drawEvent = {
+      x = pointer.x,
+      y = pointer.y,
+      type = 'MOVE_POINTER'
+    }
 
     if (drawBtnPressed) then
       if (firstDrawBtnPress) then
-        msgType = 'DRAW_START'
+        drawEvent.type = 'DRAW_START'
         firstDrawBtnPress = false
       else
-        msgType = 'DRAW'
+        drawEvent.type = 'DRAW'
       end
     else
       firstDrawBtnPress = true
     end
 
-    sendMessage(msgType)
+    sendRaw(drawEvent)
   end
 end
 
